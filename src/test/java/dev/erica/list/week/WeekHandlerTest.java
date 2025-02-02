cc77ce4fa53ed69be35a5b6e99d092a67911af8e
erikamc99 <erikamc99@gmail.com>
2025-02-02 19:16:31 +0100

    testGetDayList failed

--- src/test/java/dev/erica/list/week/WeekHandlerTest.java
+++ src/test/java/dev/erica/list/week/WeekHandlerTest.java
@@ -25,60 +25,63 @@ public class WeekHandlerTest {
     @Test
     @DisplayName("Test para crear la semana")
     public void testCreateList() {
-        WeekHandler days = new WeekHandler();
+        WeekHandler week = new WeekHandler();
         days.createList();
 
-        assertThat(days.getDayList(), containsInAnyOrder(
+        assertThat(week.getDayList(), containsInAnyOrder(
             "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
     
     }
-/*
     @Test
     @DisplayName("Test para obtener los días de la semana")
     public void testGetDayList() {
-            WeekList days = new WeekList();
-            
+        WeekHandler week = new WeekHandler();
+        week.createList();
+        List<String> days = week.getDayList();
+        assertThat(days, containsInAnyOrder(
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));         
     }
-
+            
+/*
     @Test
     @DisplayName("Test para obtener la longitud de la lista")
     public void testGetSizeList() {
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
 
     @Test
     @DisplayName("Test para oeliminar un día de la semana")
     public void testDeleteDay() {
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
 
+    @Test
+    @DisplayName("Test para obtener un día específico día de la semana")
     public void testGetSpecificDay() {
-        @Test
-        @DisplayName("Test para obtener un día específico día de la semana")
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
 
     @Test
     @DisplayName("Test para comprobar que existe un día concreto de la semana")
     public void testDayExists() {
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
 
+    @Test
+    @DisplayName("Test para ordenar alfabéticamente la lista")
     public void testSortList() {
-        @Test
-        @DisplayName("Test para ordenar alfabéticamente la lista")
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
 
     @Test
     @DisplayName("Test para vaciar la lista")
     public void testCleanList() {
-            WeekList days = new WeekList();
+        WeekHandler week = new WeekHandler();
             
     }
  */
